523a624acb3230667c6677baa210e1c0f28d9d32
carolsouza <caah.souza@live.com>
2022-10-13 21:34:48 -0300

    triagens controller + alterações users controller

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -7,102 +7,107 @@ import * as jwt from 'jsonwebtoken';
 
 
 export const getUsers = async (request: Request, response: Response) => {
-    response.json({
-        message: 'testando'
-    });
-    console.log('ok')
-    //const Users = await getRepository(Users).find();    
-    // return response.send({id: request.params.id});
+
+    let Users = await getRepository(Usuarios).find();
+
+    Users = Users.map((user) => { delete user.senha; delete user.token; return user })
+
+    return response.json(Users)
 }
 
 
 export const getUser = async (request: Request, response: Response) => {
     const { id } = request.params
 
-    const user = await getRepository(Usuarios).findOne(id);    
+    const user = await getRepository(Usuarios).findOne(id);
     return response.json(user);
 }
 
 
 export const saveUser = async (request: Request, response: Response) => {
-    console.log(request.body)
-    const hashPassword = await bcrypt.hash(request.body.values.senha, 10);
+    // console.log(request.body)
+    const hashPassword = await bcrypt.hash(request.body.senha, 10);
     const users = new Usuarios();
-    users.nome = request.body.values.nome;
+    users.nome = request.body.nome;
     users.senha = hashPassword;
-    users.dataNascimento = request.body.values.dataNascimento;
-    users.genero = request.body.values.genero;
-    users.UF = request.body.values.UF;
-    users.cidade = request.body.values.cidade;
-    users.email = request.body.values.email;
+    users.dataNascimento = request.body.dataNascimento;
+    users.genero = request.body.genero;
+    users.UF = request.body.UF;
+    users.cidade = request.body.cidade;
+    users.email = request.body.email;
 
-    const user = await getRepository(Usuarios).save(users);    
+    const user = await getRepository(Usuarios).save(users);
     response.json(user);
 }
 
-export const updateUser = async(request: Request, response: Response) => {
+export const updateUser = async (request: Request, response: Response) => {
     const { id } = request.params
 
     const hashPassword = await bcrypt.hash(request.body.senha, 10);
     const users = new Usuarios();
     users.nome = request.body.nome;
     users.senha = hashPassword;
+    users.email = request.body.email;
 
     const user = await getRepository(Usuarios).update(id, users);
 
-    if(user.affected == 1){
+    if (user.affected == 1) {
         const userUpdated = await getRepository(Usuarios).findOne(id)
         return response.json(userUpdated)
     }
 
-    return response.status(404).json({message: 'Usuário não encontrado'})
+    return response.status(404).json({ message: 'Usuário não encontrado' })
 }
 
-export const removeUser = async(request: Request, response: Response) => {
+export const removeUser = async (request: Request, response: Response) => {
     const { id } = request.params
 
     const user = await getRepository(Usuarios).delete(id);
 
-    if(user.affected == 1){
+    if (user.affected == 1) {
         const userUpdated = await getRepository(Usuarios).findOne(id)
-        return response.json({message: 'Usuário removido'})
+        return response.json({ message: 'Usuário removido' })
     }
 
-    return response.status(404).json({message: 'Usuário não encontrado'})
+    return response.status(404).json({ message: 'Usuário não encontrado' })
 }
 
-export const verificaLogin = async(request: Request, response: Response) => {
-    
+export const verificaLogin = async (request: Request, response: Response) => {
+
     if (request.body) {
-        
-        const { values } = request.body;
+
+        const values = request.body;
         const users = await getRepository(Usuarios).findOne({
             where: {
                 email: values.email
             }
-        })  
-        
-        if(users == null){
+        })
+
+        if (users == null) {
             return response.status(400).send('Nenhum usuário encontrado!')
         }
-    
+
         const isValid = await bcrypt.compare(values.senha, users.senha)
-    
-        if(!isValid){
+
+        if (!isValid) {
             return response.sendStatus(401)
         }
-    
-        const token = jwt.sign({id: users.id}, 'secret', {expiresIn: '2d'})
-    
+
+        const token = jwt.sign({ id: users.id }, 'secret', { expiresIn: '2d' })
+
+        const usersToken = { ...users, token: token }
+
+        const res = await getRepository(Usuarios).save(usersToken)
+
         delete users.senha;
-    
+
         return response.json({
             users,
             token
         })
 
     } else {
-        response.status(400).json({message: 'Requisicao Inválida!'})
+        response.status(400).json({ message: 'Requisicao Inválida!' })
     }
 
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
-import { saveUser, verificaLogin } from "./controllers/UsersController";
+import { getUser, getUsers, removeUser, saveUser, updateUser, verificaLogin } from "./controllers/UsersController";
+import { removeTriagem, getTriagem, getTriagens, saveTriagem } from "./controllers/TriagensController";
 
 const routes = Router();
 
+//rotas de usuário
 routes.post('/auth', verificaLogin)
 routes.post('/users', saveUser)
+routes.get('/users', getUsers)
+routes.get('/users/:id', getUser)
+routes.patch('/users/:id', updateUser)
+routes.delete('/users/:id', removeUser)
+
+//rotas de triagem
+routes.post('/triagem', saveTriagem)
+routes.get('/triagem', getTriagens)
+routes.get('/triagem/:id', getTriagem)
+routes.delete('/triagem/:id', removeTriagem)
 
 
 export default routes;
