d1a5a6602a71af8233c0079cc0d5b446bf286d7b
carolsouza <caah.souza@live.com>
2022-11-27 23:39:58 -0300

    adicionado rotas e controllers anamnese e consulta

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,8 @@ import {
   verificaLogin,
 } from "./controllers/UsersController";
 import { webhookComunication } from "./controllers/WebhookController";
+import { getConsultas, saveConsultas } from "./controllers/ConsultaController";
+import { getAnamnese, saveAnamnese } from "./controllers/AnamneseController";
 
 const routes = Router();
 
@@ -18,6 +20,14 @@ routes.get("/users", getUsers);
 routes.get("/users/:id", getUser);
 routes.patch("/users/:id", updateUser);
 routes.delete("/users/:id", removeUser);
+
+//rotas de consultas
+routes.get("/consultas", getConsultas);
+routes.post("/consultas", saveConsultas);
+
+//rotas de anamnese
+routes.get("/anamnese", getAnamnese);
+routes.post("/anamnese", saveAnamnese);
 
 routes.post("/webhook", webhookComunication);
 routes.get("/", (req, res) => {
